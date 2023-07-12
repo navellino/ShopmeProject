96c1bbb670840f9d5505d8f918970531a6945255
navellino <navellino@gmail.com>
2023-07-12 05:27:26 +0200

    implementazione export to csv

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/it/shopme/admin/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package it.shopme.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 2986798344527058395L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+
+	}
+	
+}
